90f708623fc61b0f256b71c63e88b654230c5ab9
Robert-Carcases <robertcarcases@gmail.com>
2024-09-16 21:56:16 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 4000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const latitude = "52";
+const longitude = "13";
+
+app.get("/", async (req, res) => {
+    try{
+        const result = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1`);
+        console.log(result.data);
+        res.render("index.ejs", {
+            time: "morning",
+            city: "testCity",
+            temperature: result.data.current.temperature_2m + " " + result.data.current_units.temperature_2m,
+            condition: "testCond"
+        });
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
