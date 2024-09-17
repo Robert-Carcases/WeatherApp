aabe591d896cb5f4f3b99d280a19016935e4e422
Robert-Carcases <robertcarcases@gmail.com>
2024-09-17 11:58:09 -0400

    Incorporated Lat/Long with a Geocoding API to display location name and time

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 4000;
+const APIkey = "b57caeaac1312e26dca51ad2d899353b";
 
 app.use(express.static(__dirname + '/public'));
 
@@ -22,13 +23,26 @@ app.post("/weather", async (req, res) => {
     console.log(req.body);
     try{
         const result = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1`);
+
+        const locResult = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${APIkey}`)
+        
+        console.log(locResult.data);
         console.log(result.data);
-        res.render("index.ejs", {
-            time: "morning",
-            city: "testCity",
+
+        const myLocation = locResult.data[0].name;
+        const dateTimeString = result.data.current.time;
+        const [date, time] = dateTimeString.split('T');
+
+        console.log(myLocation);
+
+        const weatherData = {
+            greeting: "morning",
+            time: date + " " + time,
+            city: myLocation,
             temperature: result.data.current.temperature_2m + " " + result.data.current_units.temperature_2m,
-            condition: "testCond"
-        });
+            condition: "TestCond"
+        }
+        res.json(weatherData);
     } catch (error) {
         console.log(error.response.data);
         res.status(500);
